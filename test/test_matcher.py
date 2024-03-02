zaufi/pytest-matcher

commit 64212b63b4f36c4d2d4e8dce50d899fe86e0ab7b

fix: `<test-module-name>` has never used in an expectation file path (#19)
Unfortunately, this is a breaking change… ;-(
But can be mitigate by the upcoming commits ;-)

src/matcher/plugin.py
@@ -187,6 +187,8 @@ def _make_expected_filename(
     if not result.exists():
         pytest.skip(f'Base directory for pattern-matcher do not exists: `{result}`')
 
+    result /= request.module.__name__.split('.')[-1]
+
     if request.cls is not None:
         result /= request.cls.__name__
 

test/test_matcher.py
@@ -3,11 +3,39 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
+# Standard imports
+import pathlib
+from dataclasses import dataclass
 from typing import Final
 
+# Third party packages
 import pytest
 
 pytest_plugins = ['pytester']
+
+
+@dataclass
+class _ExpectDir:
+    path: pathlib.Path
+
+    def makepatternfile(self, ext: str, **kwargs: str) -> pathlib.Path:
+        ret = None
+        for name, content in kwargs.items():
+            # NOTE Getting an indentation (to remove) by the
+            # first line in the given snippet!
+            if content[0].isspace():
+                indent = len(content) - len(content.lstrip()) - 1
+                content = '\n'.join(                        # NOQA: PLW2901
+                    line[indent:]
+                    for line in content.splitlines()
+                  ).strip()
+            p = (self.path / name).with_suffix(ext)
+            p.write_text(content)
+            if ret is None:
+                ret = p
+        assert ret is not None
+        return ret
+
 
 @pytest.fixture()
 def ourtestdir(pytester: pytest.Pytester) -> pytest.Pytester:
@@ -21,6 +49,13 @@ def ourtestdir(pytester: pytest.Pytester) -> pytest.Pytester:
         """
       )
     return pytester
+
+
+@pytest.fixture()
+def expectdir(pytester: pytest.Pytester, request: pytest.FixtureRequest) -> _ExpectDir:
+    path = pytester.path / request.function.__name__
+    path.mkdir()
+    return _ExpectDir(path)
 
 
 def no_file_test(ourtestdir) -> None:
@@ -40,9 +75,9 @@ def no_file_test(ourtestdir) -> None:
       ])
 
 
-def simple_test(ourtestdir) -> None:
+def simple_test(ourtestdir, expectdir) -> None:
     # Write a sample expectations file
-    ourtestdir.makefile('.out', test_sample_out='Hello Africa!')
+    expectdir.makepatternfile('.out', test_sample_out='Hello Africa!')
     # Write a sample test
     ourtestdir.makepyfile("""
         def test_sample_out(capfd, expected_out):
@@ -58,9 +93,9 @@ def simple_test(ourtestdir) -> None:
     result.assert_outcomes(passed=1)
 
 
-def failed_test(ourtestdir) -> None:
+def failed_test(ourtestdir, expectdir) -> None:
     # Write a sample expectations file
-    ourtestdir.makefile('.out', test_not_matched='Hello Africa!')
+    expectdir.makepatternfile('.out', test_not_matched='Hello Africa!')
     # Write a sample test
     ourtestdir.makepyfile("""
         def test_not_matched(capfd, expected_out):
@@ -83,9 +118,9 @@ def failed_test(ourtestdir) -> None:
       ])
 
 
-def regex_match_test(ourtestdir) -> None:
+def regex_match_test(ourtestdir, expectdir) -> None:
     # Write a sample expectations file
-    ourtestdir.makefile('.out', test_sample_out='.*Africa!\nHello\\s+.*!\n')
+    expectdir.makepatternfile('.out', test_sample_out='.*Africa!\nHello\\s+.*!\n')
     # Write a sample test
     ourtestdir.makepyfile("""
         def test_sample_out(capfd, expected_out):
@@ -102,9 +137,9 @@ def regex_match_test(ourtestdir) -> None:
     result.assert_outcomes(passed=1)
 
 
-def regex_fail_match_test(ourtestdir) -> None:
+def regex_fail_match_test(ourtestdir, expectdir) -> None:
     # Write a sample expectations file
-    ourtestdir.makefile('.out', test_sample_out='.*Africa!\nEhlo\\s+.*!\n')
+    expectdir.makepatternfile('.out', test_sample_out='.*Africa!\nEhlo\\s+.*!\n')
     # Write a sample test (finally)
     ourtestdir.makepyfile("""
         def test_sample_out(capfd, expected_out):
@@ -166,11 +201,11 @@ def expected_yaml_not_found_test(ourtestdir) -> None:
       ])
 
 
-def yaml_match_test(ourtestdir) -> None:
+def yaml_match_test(ourtestdir, expectdir) -> None:
     # Write a sample result and expected file
     ourtestdir.makefile(
         '.yaml'
-      , expected_yaml="""
+      , test_yaml="""
         some-key: some-value
         another-key: another-value
         simple-array:
@@ -179,7 +214,7 @@ def yaml_match_test(ourtestdir) -> None:
           - tiga
         """
       )
-    ourtestdir.makefile(
+    expectdir.makepatternfile(
         '.yaml'
       , test_yaml="""
         another-key: another-value
@@ -202,7 +237,7 @@ def yaml_match_test(ourtestdir) -> None:
     result.assert_outcomes(passed=1)
 
 
-def yaml_match_failure_test(ourtestdir) -> None:
+def yaml_match_failure_test(ourtestdir, expectdir) -> None:
     # Write a sample result and expected file
     ourtestdir.makefile(
         '.yaml'
@@ -215,7 +250,7 @@ def yaml_match_failure_test(ourtestdir) -> None:
           - tiga
         """
       )
-    ourtestdir.makefile(
+    expectdir.makepatternfile(
         '.yaml'
       , test_yaml="""
         another-key: another-value
@@ -244,7 +279,7 @@ def yaml_match_failure_test(ourtestdir) -> None:
       ])
 
 
-def parametrized_case_test(ourtestdir) -> None:
+def parametrized_case_test(ourtestdir, expectdir) -> None:
     # Given testing argvalues and expected decorations
     testing_pairs = [
         ((0, 'y'), '[0-y]'),
@@ -253,7 +288,7 @@ def parametrized_case_test(ourtestdir) -> None:
     ]
     # Write sample expectation files
     for values, decoration in testing_pairs:
-        (ourtestdir.path / f'test_parametrized{decoration}.out').write_text(str(values) + '\n')
+        (expectdir.path / f'test_parametrized{decoration}.out').write_text(str(values) + '\n')
 
     # Write a sample test
     ourtestdir.makepyfile(f"""

commit 003eec8b0be30d72bbcff73bfe5d205305194306

feat: introduce `pm-pattern-file-fmt` configuration parameter
The format string may have the following placeholders:
- `{module}`
- `{class}`
- `{fn}`
- `{callspec}`
- `{suffix}`

Default value is: `{module}/{class}/{fn}{callspec}{suffix}`.
Set it to `{class}/{fn}{callspec}{suffix}` can help existed code
to preserve backward compatibility broken by the PR #19.

pyproject.toml
@@ -217,6 +217,7 @@ select = ["ALL"]
 "test/test_*.py" = [
     "ANN001"    # Missing type annotation for function argument
   , "D"         # Documentation issues
+  , "PLR0913"   # Too many arguments in function definition
   , "PLR2004"   # Magic value used in comparison
   ]
 

src/matcher/plugin.py
@@ -8,6 +8,7 @@
 from __future__ import annotations
 
 # Standard imports
+import functools
 import os
 import pathlib
 import platform
@@ -152,6 +153,17 @@ def _try_module_path(request: pytest.FixtureRequest) -> tuple[pathlib.Path, bool
     return (pathlib.Path(request.path.parent) / 'data' / 'expected', False)
 
 
+def _subst_pattern_parts(result: pathlib.Path, current: str, **kwargs: str) -> pathlib.Path:
+    # ATTENTION If pattern format started w/ `/`
+    # (or containing it in any place), just ignore it!
+    # ATTENTION Directory traversal also just SILENTLY ignored!
+    # TODO Or must be alerted?
+    if current in ('/', '..'):
+        return result
+    part = current.format(**kwargs)
+    return result / part if part else result
+
+
 def _make_expected_filename(
     request: pytest.FixtureRequest
   , ext: str
@@ -187,18 +199,22 @@ def _make_expected_filename(
     if not result.exists():
         pytest.skip(f'Base directory for pattern-matcher do not exists: `{result}`')
 
-    result /= request.module.__name__.split('.')[-1]
+    subst: dict[str, str] = {
+        'module': request.module.__name__.split('.')[-1]
+      , 'class': request.cls.__name__ if request.cls is not None else ''
+      , 'fn': request.function.__name__
+      , 'callspec': urllib.parse.quote(
+            request.node.name[len(request.function.__name__):]
+          , safe='[]'
+          )
+      , 'suffix': '-' + platform.system() if use_system_suffix else ''
+      }
 
-    if request.cls is not None:
-        result /= request.cls.__name__
-
-    result /= (                                             # type: ignore[operator]
-        urllib.parse.quote(request.node.name, safe='[]')
-      + ('-' + platform.system() if use_system_suffix else '')
-      + ext
-      )
-
-    return result
+    return functools.reduce(
+        functools.partial(_subst_pattern_parts, **subst)
+      , pathlib.Path(request.config.getini('pm-pattern-file-fmt')).parts
+      , result
+      ).with_suffix(ext)
 
 
 @pytest.fixture()
@@ -382,6 +398,12 @@ def pytest_addoption(parser: pytest.Parser) -> None:
       , help='expect a system name (`platform.system()`) to be a pattern filename suffix'
       , type='bool'
       , default=False
+      )
+    parser.addini(
+        'pm-pattern-file-fmt'
+      , help='pattern filename format can use placeholders: `module`, `class`, `fn`, `callspec`, `system`'
+      , type='string'
+      , default='{module}/{class}/{fn}{callspec}{suffix}'
       )
 
 

test/test_matcher.py
@@ -340,3 +340,68 @@ def reveal_unused_files_test(return_codes, expected_code, ourtestdir, monkeypatc
         f'{(ourtestdir.path / p)!s}'
         for p in ('test_a.err', 'test_b.out')
       ])
+
+
+@pytest.mark.parametrize(
+    ('fmt', 'file_name', 'cls_name', 'fn_name', 'expected_path')
+  , [
+        # Format w/ just a function name produces an expectation file in the base dir
+        ('{fn}',                 'simple_test',     None,      'test_fn', 'test_fn.out')
+        # Any leading `/` doesn't produce any files in the FS root
+      , ('/{fn}',                'simple_test',     None,      'test_fn', 'test_fn.out')
+        # Any trailing slashes also ignored
+      , ('{fn}/',                'simple_test',     None,      'test_fn', 'test_fn.out')
+        # Play w/ other components...
+      , ('{module}-{fn}',        'simple_test',     None,      'test_fn', 'simple_test-test_fn.out')
+      , ('{module}/{fn}',        'subdir_test',     None,      'test_fn', 'subdir_test/test_fn.out')
+        # ... patterns may include some static text
+      , ('py-{module}/exp-{fn}', 'pfx_subdir_test', None,      'test_fn', 'py-pfx_subdir_test/exp-test_fn.out')
+        # Missed class name means no corresponding subdir
+      , ('{class}/{fn}',         'subdir_test',     None,      'test_fn', 'test_fn.out')
+        # Missed class name in the format is Ok
+      , ('{fn}',                 'simple_test',     'TestCls', 'test_fn', 'test_fn.out')
+        # Play w/ other components...
+      , ('{class}+{fn}',         'simple_test',     'TestCls', 'test_fn', 'TestCls+test_fn.out')
+      , ('{module}/{class}/{fn}','simple_test',     'TestCls', 'test_fn', 'simple_test/TestCls/test_fn.out')
+        # Redundant '/' just ignored
+      , ('{module}//{class}//{fn}','simple_test',   'TestCls', 'test_fn', 'simple_test/TestCls/test_fn.out')
+        # '.' is meaningless
+      , ('./{class}/{fn}',         'simple_test',   'TestCls', 'test_fn', 'TestCls/test_fn.out')
+      , ('{class}/./{fn}',         'simple_test',   'TestCls', 'test_fn', 'TestCls/test_fn.out')
+        # Directory traversal also ignored
+      , ('../{class}/../{fn}',     'simple_test',   'TestCls', 'test_fn', 'TestCls/test_fn.out')
+    ]
+  )
+def fn_fmt_test(pytester: pytest.Pytester, fmt, file_name, cls_name, fn_name, expected_path) -> None:
+    # Write a sample config file
+    pytester.makefile(
+        '.ini'
+      , pytest=f"""
+            [pytest]
+            addopts = -vv -ra
+            pm-patterns-base-dir = {pytester.path!s}
+            pm-pattern-file-fmt = {fmt}
+        """
+      )
+
+    # Write a sample expectations file
+    path = pytester.path / expected_path
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_text('Hello Africa!')
+
+    # Write a sample test
+    indent = ' ' * (4 if cls_name is not None else 0)
+    kwargs = {file_name: f"""
+        {('class ' + cls_name + ':') if cls_name is not None else ''}
+        {indent}def {fn_name}({'self, ' if cls_name is not None else ''}capfd, expected_out):
+        {indent}    print('Hello Africa!', end='')
+        {indent}    stdout, stderr = capfd.readouterr()
+        {indent}    assert expected_out == stdout
+        {indent}    assert stderr == ''
+        """
+      }
+    pytester.makepyfile(**kwargs)
+
+    # Run all tests with pytest
+    result = pytester.runpytest()
+    result.assert_outcomes(passed=1)