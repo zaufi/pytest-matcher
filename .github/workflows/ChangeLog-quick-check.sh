2915611f7c899dbd01766df007c70122f99a5ee1
Alex Turbov <i.zaufi@gmail.com>
2024-03-11 10:53:33 +0400

    impr(release-workflow): stricter way to check the `ChangeLog.rst`

--- .github/workflows/ChangeLog-quick-check.sh
+++ .github/workflows/ChangeLog-quick-check.sh
@@ -7,9 +7,10 @@ set -e
 # NOTE Not far than 5 lines below this marker should
 # be a top (now released) version title.
 declare -r MARKER='.. program:: pytest-matcher'
-declare -r VERSION_RE="^${SETUPTOOLS_SCM_PRETEND_VERSION:?}_ -- 20[0-9][0-9]-[0-9][0-9]-[0-9][0-9]$"
+declare -r VERSION_RE="^[0-9]+\.[0-9]+\.[0-9]+_ -- 20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]$"
+declare -r PRETEND_RE="${SETUPTOOLS_SCM_PRETEND_VERSION:?${SETUPTOOLS_SCM_PRETEND_VERSION//./\\.}}"
 
-if grep -A 5 "${MARKER}" ChangeLog.rst | grep -q "${VERSION_RE}"; then
+if grep -A 5 "${MARKER}" ChangeLog.rst | grep -E "${VERSION_RE}" | grep -q "${PRETEND_RE}"; then
     echo "$0: ✔ Found ${SETUPTOOLS_SCM_PRETEND_VERSION} on top"
 else
     echo "$0: ❌ Error: ChageLog must be updated to include release ${SETUPTOOLS_SCM_PRETEND_VERSION}!" >&2
